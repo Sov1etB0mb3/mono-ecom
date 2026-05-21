aca72fc7b9e6a2e13b2d236d626a2746d9804c79
Alan <nguyentrungkhanh804@gmail.com>
2026-05-21 13:45:03 +0700

    add paginate, change title

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,25 +1,31 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpHeaders } from '@angular/common/http';
 
 import SharedModule from 'app/shared/shared.module';
+import { ItemCountComponent } from 'app/shared/pagination';
 import { LoginService } from 'app/login/login.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
 import { ProductService } from 'app/entities/product/service/product.service';
 import { CartService } from 'app/cart/cart.service';
 import { IProduct } from 'app/entities/product/product.model';
+import { ITEMS_PER_PAGE, TOTAL_COUNT_RESPONSE_HEADER } from 'app/config/pagination.constants';
 import { finalize } from 'rxjs';
 
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [SharedModule, RouterModule],
+  imports: [SharedModule, RouterModule, ItemCountComponent],
 })
 export default class HomeComponent implements OnInit {
   account = signal<Account | null>(null);
   products = signal<IProduct[]>([]);
   isLoading = signal(false);
+  page = 1;
+  totalItems = 0;
+  readonly itemsPerPage = ITEMS_PER_PAGE;
 
   private readonly accountService = inject(AccountService);
   private readonly loginService = inject(LoginService);
@@ -35,10 +41,24 @@ export default class HomeComponent implements OnInit {
 
   loadProducts(): void {
     this.isLoading.set(true);
+    const queryObject = {
+      page: this.page - 1,
+      size: this.itemsPerPage,
+      eagerload: true,
+      sort: 'id,asc',
+    };
     this.productService
-      .query()
+      .query(queryObject)
       .pipe(finalize(() => this.isLoading.set(false)))
-      .subscribe(res => this.products.set(res.body ?? []));
+      .subscribe(res => {
+        this.products.set(res.body ?? []);
+        this.totalItems = Number(res.headers.get(TOTAL_COUNT_RESPONSE_HEADER));
+      });
+  }
+
+  navigateToPage(page: number): void {
+    this.page = page;
+    this.loadProducts();
   }
 
   addToCart(product: IProduct): void {
