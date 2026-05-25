1fc7d1973b93af514eb3f5ab66aeecee6d88d9dd
Alan <nguyentrungkhanh804@gmail.com>
2026-05-25 14:54:48 +0700

    change view scope into name instead of id, join table to get scope name instead id only

--- src/main/java/com/calt/buroxz/repository/AuthorityScopeLinkerRepository.java
+++ src/main/java/com/calt/buroxz/repository/AuthorityScopeLinkerRepository.java
@@ -1,7 +1,10 @@
 package com.calt.buroxz.repository;
 
 import com.calt.buroxz.domain.AuthorityScopeLinker;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +12,10 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface AuthorityScopeLinkerRepository extends JpaRepository<AuthorityScopeLinker, Long> {}
+public interface AuthorityScopeLinkerRepository extends JpaRepository<AuthorityScopeLinker, Long> {
+    @Query("SELECT asl FROM AuthorityScopeLinker asl LEFT JOIN FETCH asl.authority LEFT JOIN FETCH asl.scope")
+    List<AuthorityScopeLinker> findAll();
+
+    @Query("SELECT asl FROM AuthorityScopeLinker asl LEFT JOIN FETCH asl.authority LEFT JOIN FETCH asl.scope WHERE asl.id = :id")
+    Optional<AuthorityScopeLinker> findById(@Param("id") Long id);
+}
